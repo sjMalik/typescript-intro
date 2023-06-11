fc07b3efcd805f62c5e2420691d4c88720dc6ba0
sjmalik <surajit.malik@qlikchain.com>
2023-06-11 11:34:55 +0530

    init

--- example.ts
+++ example.ts
@@ -26,6 +26,25 @@ function add2(a: number, b: number) {
 
 // Inferred Types
 
+/**
+ * Typescript know that the myName2 is string because the right hand side is a string assignment.
+ * It has infered the type from the right hand side. As the right hand side has a specific types then the variables will be created with the specific type
+ */
+let myName2 = "Surajit";
+let isCool2 = true;
+let favNumber2 = 42;
+
+// This is compile time error. Its not affecting the actual generated code thats going to be running for your application. 
+// It generates the error at compile time when its turning typescript into javascript.
+myName2 = 2; // Error: type number is not assignable to type string
+
+function add3(a: number, b: number) {
+    return a + b;
+}
+
+let result = add3(1, 2);
+result = 'Sura';    // Error: type string is not assignable to type number
+
 // Union Types
 
 // Objects and Interfaces
